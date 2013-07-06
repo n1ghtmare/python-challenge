8d65aa520d3fc1c25d9b12e94a9c9e2cb66a074c
Dimitar Dimitrov <dimitar@nightosx.local>
2013-07-06 21:08:44 +0400

    Added solution for level 7

---
+++ Level 7/decode_image.py
@@ -0,0 +1,26 @@
+# -> use PIL to decypher the pixel colors at the grey bar
+import string
+import Image
+
+def read_image_colors(file_name):
+	im = Image.open(file_name)
+	pic = im.load()
+	(width, height) = im.size
+	colors = []
+	y = height / 2
+	for i in range(width /7):		
+		color = pic[i * 7,y][0]
+		colors.append(color)
+	return colors
+
+def decipher(items):
+	message = ""
+	for i in items:
+		message += chr(i)
+	return message
+
+if __name__ == "__main__":
+	colors = read_image_colors("oxygen.png")
+	print decipher(colors)
+	print decipher([105, 110, 116, 101, 103, 114, 105, 116, 121])
+	
